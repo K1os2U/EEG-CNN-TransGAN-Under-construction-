dbc8418e4cc3fb63ed8aa1419990ddc907ffa396
K1os2U <2486672884@qq.com>
2026-03-31 22:22:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-import pandas as pd
-print(pd.__version__)
 
-s = pd.Series([[1, 3, 5, 7],[15,5,2,1]], name='numbers')
-print(s)  # 输出前两行
-s.index = ['a', 'b']
-print(s)
-
-data = {'Name': ['Alice', 'Bob'], 'Age': [30,25],'Gender':[1,5]}
-df = pd.DataFrame(data)
-df['Age'] = df['Age'].astype('float')
-print(df.sort_values(by='Age', ascending=False, inplace=True))
-print(df)
-print(df.isnull().sum())
-print(df.groupby('Gender')['Age'].mean())
-print(df.describe())
